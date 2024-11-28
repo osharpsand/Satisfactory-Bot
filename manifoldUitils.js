1b9eadc6fb2003a29faae877e4048c85022ddd40
osharpsand <osharpsand@gmail.com>
2024-11-28 15:52:29 -0700

    Update manifoldUitils.js

--- manifoldUitils.js
+++ manifoldUitils.js
@@ -1,14 +1,34 @@
-function CalculateCompleteManifoldData(machineName, machinePower, itemName, outputSpeed, processSpeed, maxBeltSpeed, inputItems, inputItemsSpeed) {
-  // machines/ injectors outputs/
+function CalculateCompleteManifoldData(machineName, machinePower, itemName, outputSpeed, processSpeed, maxBeltSpeed, inputItems) {
+  // machines/ inputs outputs/ message
 
+  //Message
+  let message = `Complete Breakdown:\n`;
+  
   //Machines
   const machinesNeeded = Math.floor(outputSpeed / processSpeed);
   const remainderOfItemsToMachine = outputSpeed % processSpeed;
-  const percentLeftOfmachine = remainderOfItemsToMachine / processSpeed * 100;
+  const percentLeftOfMachine = remainderOfItemsToMachine / processSpeed * 100;
   const machinePowerEstimate = outputSpeed / processSpeed * machinePower;
+
+  if ( percentLeftOfMachine === 0 ) {
+    message += `You need ${machinesNeeded} ${machineName}(s)\n`;
+  } else {
+    message += `You need ${machinesNeeded}  ${machineName}(s) and 1 ${machineName} running at ${percentLeftOfMachine}%\n`;
+  }
 
   //Outputs
   const outputBeltsNeeded = Math.floor(outputSpeed / maxBeltSpeed);
-  const remainderOfItemsLeft = 0;
+  const remainderOfItemsLeft = outputSpeed % maxBeltSpeed;
+
+  if ( remainderOfItemsLeft === 0 ) {
+    
+  }
   
+  //Inputs
+
+  inputItems.foreach(ItemData => {
+    
+    
+    
+  })
 }
